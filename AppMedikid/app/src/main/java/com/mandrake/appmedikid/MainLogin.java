510278edf5789d8f1c0e1339ed7b881ca2d4655d
WMandraque <cristhianp00@gmail.com>
2015-06-22 13:22:53 -0500

    1
    1

---
+++ AppMedikid/app/src/main/java/com/mandrake/appmedikid/MainLogin.java
@@ -0,0 +1,76 @@
+package com.mandrake.appmedikid;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+
+public class MainLogin extends ActionBarActivity
+{
+
+    private Button btnEntrar, btnRegistrar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_login);
+
+
+        btnEntrar=(Button)findViewById(R.id.btnEntrar);
+        btnRegistrar=(Button)findViewById(R.id.btnRegistrar);
+
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+
+        btnEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainLogin.this, MainPrincipal.class));
+            }
+        });
+
+        btnRegistrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                startActivity(new Intent(MainLogin.this, MainRegistro.class));
+            }
+        });
+
+    }
+
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main_login, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ AppMedikid/app/src/main/java/com/mandrake/appmedikid/MainPrincipal.java
@@ -0,0 +1,77 @@
+package com.mandrake.appmedikid;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+
+public class MainPrincipal extends ActionBarActivity
+{
+    private Button btnSalir, btnReserva, btnAtencion;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_principal);
+
+        btnSalir=(Button)findViewById(R.id.btnSalir);
+        btnReserva=(Button)findViewById(R.id.btnReserva);
+        btnAtencion=(Button)findViewById(R.id.btnAtencion);
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+
+        btnSalir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainPrincipal.this, MainLogin.class));
+            }
+        });
+
+
+        btnReserva.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(MainPrincipal.this, MainReserva.class));
+            }
+        });
+
+        btnAtencion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainPrincipal.this, MainAtencion.class));
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main_principal, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
